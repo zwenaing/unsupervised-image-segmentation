3b1c775f898a6212503ef16b0722aa48f6ee2c8c
Zwe Naing <zwenaing.1996@gmail.com>
2018-03-27 17:07:04 -0400

    Added batch_normalization

--- autoencoder.py
+++ autoencoder.py
@@ -13,6 +13,8 @@ learning_rate = 0.0001
 num_steps = 1000
 display_step = 1
 
+global_step = 0
+
 X = tf.placeholder(tf.float32, [None, 224, 224, None])
 
 with tf.name_scope("Encoding"):
@@ -24,12 +26,14 @@ with tf.name_scope("Decoding"):
 with tf.name_scope("Loss"):
     y_pred = tf.reshape(decoded_image, [-1, 150528])
     y_true = tf.reshape(X, [-1, 150528])
-
     loss = tf.reduce_mean(tf.pow(y_pred - y_true, 2))
+tf.summary.scalar("SEE loss", loss)
 
 with tf.name_scope("Optimization"):
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     train_op = optimizer.minimize(loss=loss)
+
+merged_summary = tf.summary.merge_all()
 
 init = tf.global_variables_initializer()
 
@@ -37,18 +41,22 @@ saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(init)
+    train_writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())
+
     if exists(logdir):
-        sess = saver.restore(sess, logdir)
+        saver.restore(sess, logdir)
     iterator = input_data()
 
     for i in range(num_steps + 1):
         next_items = iterator.get_next()
         batch_x =  sess.run(next_items)
         _ = sess.run(train_op, feed_dict={X: batch_x})
+
         if i % display_step == 0:
-            loss_ = sess.run(loss, feed_dict={X: batch_x})
+            loss_, summary = sess.run([loss, merged_summary], feed_dict={X: batch_x})
             print("Iteration number: ", str(i), " Loss: ", str(loss_))
-            saver.save(sess, logdir)
+            train_writer.add_summary(summary)
+            saver.save(sess, logdir, global_step=global_step + i)
 
 
 
